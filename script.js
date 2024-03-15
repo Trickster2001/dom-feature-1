a2d5efd038538e11a0bd1d0ce907baa21665d9de
Trickster2001 <keyurmurkar@gmail.com>
2024-03-15 16:34:17 +0530

    Completed

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello World!");
+
+let isStatus = document.querySelector("h5");
+let btn = document.querySelector("#add");
+let flag = 0;
+
+btn.addEventListener("click", ()=>{
+  if(flag===0){
+    isStatus.innerHTML = "Friend";
+    isStatus.style.color = "green";
+    btn.innerHTML = "Remove Friend!"
+    flag = 1;
+  }else {
+    isStatus.innerHTML = "Stranger";
+    isStatus.style.color = "red";
+    btn.innerHTML = "Add Friend!"
+    flag = 0;
+  }
+})
